b5adeb524cfd4f5f43a3449ea8134dc6814b09d8
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-05 00:43:42 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+  if speed < 40 || speed > 60
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	not_safe = speed < 40 || speed > 60
+	not_safe ? true : false
 end
 	
 
